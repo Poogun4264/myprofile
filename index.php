2db6a662361afee0672f30402d7c833490560fad
LAPTOP-6NQ3UMQH\poogun <wipadragon64@gmail.com>
2024-10-25 02:40:37 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    <title>My Profils</title>
+
+</head>
+
+<body>
+
+    <div class="container" style="background-color: rgba(57, 109, 88, 0.651);">
+        <nav class="navbar">
+            <h1>Personal Profile</h1>
+        </nav>
+        <hr>
+        <div class="content m-3">
+            <div class="row">
+                <div class="col-md-6">
+                    <!-- detail L-->
+                    <div class="row">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="d-flex flex-column align-items-center text-center">
+                                    <img src="https://scontent.fkkc3-1.fna.fbcdn.net/v/t39.30808-6/448431914_18440082118003687_7098996369500547227_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=127cfc&_nc_ohc=1X1Yhfqdqt0Q7kNvgFsY8Gb&_nc_zt=23&_nc_ht=scontent.fkkc3-1.fna&_nc_gid=A_PlkKtQDUfIXr4Gjoyzhfh&oh=00_AYDKpg5tXKeBE4Zq0epuf7CmLCI1fNIGvNjTv8bhE8K44w&oe=671EAB58" width="150">
+                                    <div class="mt-3">
+                                        <h4>Chompoo</h4>
+                                        <p class="text-muted font-size-sm">I own the jewelry brand WIWA
+                                            and runs an automated electric massage chair business.
+                                            And team up to develop programs with nextsoftware company.</p>
+                                        <button class="btn btn-primary">Follow</button>
+                                        <button class="btn btn-outline-primary">Message</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">contact</div>
+                </div>
+
+                <div class="col-md-6">
+                    <!-- detail R -->
+                    <div class="row">
+                        55555555555555555555555
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">1111111111111111111</div>
+                        <div class="col-md-6">222222222222222222</div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+</body>
+
+
+
+</html>
